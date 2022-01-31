7764784423158ccf986363503b378587b6468456
夜月Kalo <64715639+Black-Kalo@users.noreply.github.com>
2022-01-31 15:19:39 +0000

    add github link

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Discord search id website [![website](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-4LVZkfpWHbVh0xcQVgaVH1ge3US6OybX5Q&usqp=CAU)](https://search-discord-id.9688girl.repl.co/)
+## Discord search id website (https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-4LVZkfpWHbVh0xcQVgaVH1ge3US6OybX5Q&usqp=CAU)(https://search-discord-id.9688girl.repl.co/)
 
 最好的查詢Discord用戶/機器人ID網站
 
--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,7 +5,7 @@ export default function AppFooter({ visits }) {
         <footer className="site-footer">
                 <div>
                     <div>
-                        源代碼: <a href='yee' target="_blank" rel="noopener noreferrer">code</a>&nbsp;
+                        源代碼: <a href='https://github.com/Black-Kalo/search-discord-id' target="_blank" rel="noopener noreferrer">code</a>&nbsp;
                         本網站不是官方 <a className='footer-links' href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord</a>
                     </div>
                     <span>今天使用人數: <span className="count">{ visits }</span></span>
